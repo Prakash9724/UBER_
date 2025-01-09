464c61e3af617c6c2f0c6bce11f03aebb349bc80
prakash9724 <hellobrother91069@gmail.com>
2025-01-09 18:20:45 +0530

    Add user registration functionality with validation and new dependencies
    - Implement user registration endpoint in user.controller.js with input validation using express-validator.
    - Update app.js to include user routes and middleware for JSON and URL-encoded data.
    - Add express-validator dependency in package.json and package-lock.json for input validation.
    - Ensure proper error handling for validation errors during user registration.

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,13 @@ const cors = require('cors');
 app.use(cors());
 const connectToDb = require('./db/db');
 connectToDb();
+const userRoute = require('./routes/user.route');
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/user', userRoute);
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
