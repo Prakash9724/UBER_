88dea187977522bfc2c37a802da54fc5b48a8842
prakash9724 <hellobrother91069@gmail.com>
2025-01-19 19:58:39 +0530

    Enhance CaptainHome component with new layout and navigation
    - Redesigned CaptainHome to include a fixed header with logo and logout link.
    - Added a full-screen background image for improved visual appeal.
    - Introduced a user information section displaying user name and balance.
    - Improved overall structure and styling for better user experience.

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CaptainHome = () => {
   return (
-    <div>CaptainHome</div>
+    <div className="h-screen">
+    
+
+    <div className='fixed w-screen p-3 top-0 flex items-center justify-between'>
+    <img className='w-14 h-6' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+    <Link to={'/home'} className="  h-10 w-10 bg-white flex items-center justify-center rounded-full">
+    <i className="text-lg font-medium ri-logout-circle-line"></i>
+    </Link>
+    </div>
+  <div className="h-1/2">
+    <img
+      className="w-full h-full object-cover"
+      src="https://preview.redd.it/nearby-drivers-overlay-just-dropped-in-my-market-but-v0-3l3w0gu80n4d1.jpg?width=1080&crop=smart&auto=webp&s=464060bc7486e5131fd337d8cfb2fb7551347f2a"
+      alt=""
+    />
+  </div>
+  <div className="h-1/2 p-4">
+    <div>
+      <div>
+        <img src="" alt="" />
+        <h2>Prakash</h2>
+      </div>
+      <div>
+        <h4>200.20</h4>
+      </div>
+    </div>
+  </div>
+</div>
   )
 }
 
